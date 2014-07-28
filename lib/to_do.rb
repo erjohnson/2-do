f4f52cbd19e2a82819cbc041d480336cc767c242
Jeff Stringer & Eric Johnson <student@epicodus.com>
2014-07-28 10:08:18 -0700

    add List class and specs

--- lib/to_do.rb
+++ lib/to_do.rb
@@ -1,23 +1,24 @@
 class Task
 
-  attr_accessor :description, :done
+  attr_accessor :description, :done, :due_date
 
   def initialize description
     @description = description
     @done = false
-  end
-
-  def task
-    { description: @description }
+    @due_date = '5 days'
   end
 end
 
 class List
+
+  attr_accessor :name, :tasks
+
   def initialize name
     @name = name
     @tasks = []
   end
-  def add_task
 
+  def add_task description
+    @tasks << Task.new(description)
   end
 end
--- spec/to_do_spec.rb
+++ spec/to_do_spec.rb
@@ -12,13 +12,45 @@ describe Task do
     expect(test_task.description).to eq 'scrub the zebra'
   end
 
-  it 'creates a hash with contents inside' do
-    test_task = Task.new 'scrub the zebra'
-    expect(test_task.task).to eq({description: 'scrub the zebra'})
-  end
-
   it 'lets you read if the task is done' do
     test_task = Task.new 'a new task'
     expect(test_task.done).to eq(false)
+  end
+
+  it 'lets you add a due date to your task' do
+    test_task = Task.new 'a new task'
+    test_task.due_date = '5 days'
+    expect(test_task.due_date).to eq '5 days'
+  end
+end
+
+describe List do
+  it 'is initialized with a name' do
+    new_list = List.new 'Chores'
+    expect(new_list).to be_an_instance_of List
+  end
+
+  it 'lets you read the list name' do
+    new_list = List.new 'Chores'
+    expect(new_list.name).to eq 'Chores'
+  end
+
+  it 'lets you access all of the lists you created' do
+    new_list = List.new 'Chores'
+    expect(new_list.tasks).to eq []
+  end
+
+  it 'lets you add a new task to the current list' do
+    new_list = List.new 'Jobs'
+    new_list.add_task('wash the garbage')
+    expect(new_list.tasks.length).to eq 1
+  end
+
+  it 'lets you read the task description' do
+    new_list = List.new 'Jobs'
+    new_list.add_task('wash the garbage')
+    new_list.add_task('eat the dishes')
+    p new_list.tasks.inspect
+    expect(new_list.tasks.at(0).description).to eq 'wash the garbage'
   end
 end
